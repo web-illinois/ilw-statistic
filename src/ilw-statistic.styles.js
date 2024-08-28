ca4e835153886f47cddc9501957a6b345cc46053
Sydney Flowers <105298688+sydbeth@users.noreply.github.com>
2024-08-28 13:58:44 -0500

    Update ilw-statistic.styles.js

--- src/ilw-statistic.styles.js
+++ src/ilw-statistic.styles.js
@@ -1,5 +1,18 @@
 import { css } from 'lit';
 
 export default css`
-
-`;
+    :host {
+      display: block;
+      width: 100%;
+      container-type: inline-size;
+    }
+    div {
+      font-size: 1.25rem;
+      font-weight: 500;
+    }
+  @container (min-width: 50rem) {
+    div {
+      font-size: 2rem;
+    }
+  }
+  `;
